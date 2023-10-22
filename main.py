f52647a76ebed9363aec04e8c0bfc5fe0f362cab
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-10-22 20:26:40 +0800

    initialize mongodb connection

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pymongo.mongo_client import MongoClient
+
+app = FastAPI()
+
+
+uri = "mongodb+srv://admin:admin1234@fastapi-mongodb-cluster.gks8qk3.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command("ping")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
